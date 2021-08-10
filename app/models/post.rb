eeb8e1aafbd8691f60bd941baa728704fdea8c76
Mike Rogers <me@mikerogers.io>
2021-08-09 22:32:52 -0500

    Bumping RubyZip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
-  validates :title, presence: true
-  validates :body, presence: true
+  validates :title, presence: true, filter_words: ["javascript"]
+  validates :body, presence: true, filter_words: ["javascript"]
 end
